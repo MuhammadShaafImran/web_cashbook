a6ebb4688d52921f9d2a0ea553243dfbebca5776
shaaf Imran <shaafimran257@gmail.com>
2025-03-05 14:27:43 +0500

    Cashbook Diagram Added

--- cashbook/store/views.py
+++ cashbook/store/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'store/admin.html')
+    context = {
+        'username': request.user.username,
+    }
+    return render(request, 'store/admin.html',context)
 
 def seller(request):
     return render(request, 'store/seller-page.html')
